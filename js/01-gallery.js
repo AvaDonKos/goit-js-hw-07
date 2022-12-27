bc82061eeb6b3c62572beefe9854a5c85ee7ea2c
Kostiantyn Zaremba <avadonkos@gmail.com>
2022-12-27 15:59:11 +0200

    task1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,35 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+galleryItems.forEach((el) => {
+  const { preview, original, description } = el;
+
+  const galleryItem = `<div class="gallery__item">
+      <a class="gallery__link" href="${preview}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`;
+  galleryEl.insertAdjacentHTML('beforeend', galleryItem);
+});
+
+const handleOpenOriginal = (event) => {
+  event.preventDefault();
+
+  const { target } = event;
+
+  if (target.nodeName !== 'IMG') {
+    return;
+  }
+
+  basicLightbox.create(`<img src="${target.dataset.source}">`).show();
+};
+
+galleryEl.addEventListener('click', handleOpenOriginal);
